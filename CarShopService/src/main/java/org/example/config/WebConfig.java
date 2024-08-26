385d788f0b9ba1d4294d5e409789f97f5c15f03d
Solov18 <solov18@list.ru>
2024-08-26 13:10:38 +0300

    next 7.1 hw4

--- CarShopService/src/main/java/org/example/config/WebConfig.java
+++ CarShopService/src/main/java/org/example/config/WebConfig.java
@@ -19,9 +19,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/swagger-ui/")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui/");
+
+
+   //    адрес страницы    http://localhost:8080/swagger-ui/index.html
+
+
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
