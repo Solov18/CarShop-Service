1d1662e90a882cde3495ee8f8567133b95786554
Solov18 <solov18@list.ru>
2024-08-31 17:56:15 +0300

    new 5.5 hw5

--- CarShopService/audit-starter/src/main/java/org/example/logi/AuditAspect.java
+++ CarShopService/audit-starter/src/main/java/org/example/logi/AuditAspect.java
@@ -3,6 +3,7 @@ package org.example.logi;
 
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,6 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 
 import java.time.LocalDateTime;
